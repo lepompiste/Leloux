7fcbab461074348e76b10d7c460fc5c2a0163fbf
lepompiste <julienrobin71@outlook.fr>
2020-03-28 23:27:01 +0100

    Created routes and requests

--- leloux.js
+++ leloux.js
@@ -1,5 +1,7 @@
 ;(
 function(){
+	toUpdate = []
+
 	function createElement(type, props, ...children) {
 		return {
 			type: type,
@@ -22,26 +24,184 @@ function(){
 		}
 	}
 
-	function render(container, element) {
+	function renderElement(container, element) {
 		const vdom = element.type == "TEXT_ELEMENT" ? document.createTextNode(element.props.nodeValue) : document.createElement(element.type)
 		
 		Object.keys(element.props).forEach(name => {
 			if (name != "children") {
-				vdom[name] = element.props[name]
+				if (name == "events") {
+					Object.keys(element.props.events).forEach(eventName => {
+						vdom.addEventListener(eventName, element.props.events[eventName])
+					})
+				} else {
+					vdom[name] = element.props[name]
+				}
 			}
 		})
 
 		element.props.children.forEach(child => {
-			render(vdom, child)
+			renderElement(vdom, child)
 		});
 
 		container.appendChild(vdom)
 	}
 
+	function render(container, component) {
+		container.innerHTML = null
+		if (typeof component.init == "function") {
+			component.init()
+		}
+		if (typeof component.view == "function") {
+			renderElement(container, component.view())
+		}
+	}
+
+	function mount(container, component) {
+		toUpdate.forEach(mountElement => {
+			if (container == mountElement[0]) {
+				toUpdate.splice(toUpdate.indexOf(mountElement))
+			}
+		})
+		toUpdate.push([container, component])
+		render(container, component)
+	}
+
+	function redrawAll() {
+		toUpdate.forEach(el => {
+			render(el[0], el[1])
+		})
+	}
+
+	function getOngoingRedraw() {
+		return toUpdate
+	}
+
+	var routes = {
+		symbol: "#!",
+		paramSymbol: "!",
+		defaultRoute: undefined,
+		mountpoint: undefined,
+		routes: {},
+		loaded: undefined,
+		mount: true,
+
+		getParam: function () {
+			split = window.location.hash.split(routes.paramSymbol)
+			param = split.length > 2 ? split.slice(2).join(routes.paramSymbol) : ""
+			return param
+		},
+
+		load: function(route) {
+			if (route === null) {
+				console.log("ici")
+				render(routes.mountpoint, {view:function(){return l("p",{},"404 error")}})
+			} else if (route === undefined) {
+				if (routes.routes[routes.defaultRoute] != undefined) {
+					routes.load(routes.defaultRoute)
+				}
+			} else if (routes.routes[route] != undefined) {
+				if (routes.mount) {
+					mount(routes.mountpoint, routes.routes[route])
+				} else {
+					render(routes.mountpoint, routes.routes[route])
+				}
+				routes.loaded = route
+			} 
+		},
+
+		reload: function () {
+			routes.load(routes.resolve())
+		},
+
+		def: function(mountpoint, defaultRoute, _routes) {
+			routes.defaultRoute = defaultRoute
+			routes.mountpoint = mountpoint
+			routes.routes = _routes
+			routes.init()
+		},
+
+		goto: function(path) {
+			window.location.hash = routes.symbol + path
+		},
+
+		resolve: function() {
+			var found = false
+			r = null
+
+			pathRegexp = new RegExp("^" + routes.symbol)
+
+			if (pathRegexp.test(window.location.hash)) {
+				route = window.location.hash.replace(routes.symbol, "")
+
+				Object.keys(routes.routes).forEach(function(val) {
+					routeRegexp = new RegExp("(^\\" + val + "$|^\\" + val + routes.paramSymbol + ")")
+					if (routeRegexp.test(route)) {
+						found = true // found a route that match the request
+						r = val
+						return
+					}
+				})
+
+				if (!found) { // if not found, return null
+					return null
+				}
+				return r
+			}
+			return undefined // if not provided, returl undefined
+		},
+
+		onroutechange: function() {},
+		listener: function(e) {
+			routes.onroutechange()
+			routes.load(routes.resolve())
+		},
+		init: function() {
+			window.addEventListener("hashchange", routes.listener)
+			routes.listener()
+		}
+	}
+
+	var requests = {
+		make: function(mode, url, props, isJSON) {
+			return new Promise((resolve, reject) => {
+				let xhr = new XMLHttpRequest();
+				xhr.open(mode, url);
+				if (props.headers) {
+					Object.keys(props.headers).forEach(key => {
+						xhr.setRequestHeader(key, props.headers[key]);
+					});
+				}
+				xhr.onload = () => {
+					if (xhr.status >= 200 && xhr.status < 300) {
+						if (isJSON) {
+							resolve(JSON.parse(xhr.response))
+						} else {
+							resolve(xhr.response);
+						}
+					} else {
+						reject(xhr.statusText);
+					}
+				};
+				xhr.onerror = () => reject(xhr.statusText);
+				xhr.send(props.body);
+			});
+		},
+		makej: function(mode, url, props) {
+			return requests.make(mode, url, props, true)
+		}
+	}
+	
+
 	Leloux = createElement
 	Leloux.createElement = createElement
 	Leloux.createTextElement = createTextElement
+	Leloux.renderElement = renderElement
 	Leloux.render = render
+	Leloux.mount = mount
+	Leloux.redrawAll = redrawAll
+	Leloux.getOngoingRedraw = getOngoingRedraw
+	Leloux.routes = routes
+	Leloux.requests = requests
 
 	window.Leloux = Leloux
 	window.l = Leloux
